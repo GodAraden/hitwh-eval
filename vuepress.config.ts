46e546454dd996992d45a33b5a66cd255cf484c5
Araden <2713886816@qq.com>
2023-08-02 23:04:21 +0800

    feat: 添加代码复制按钮

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,7 +1,14 @@
 import { defineUserConfig } from 'vuepress'
+import CodeCopyPulgin from 'vuepress-code-copy'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'HITwh 自动评教脚本',
-  description: 'Author：Araden'
+  description: 'Author：Araden',
+  plugins: [
+    CodeCopyPulgin({
+      copyText: '点此复制',
+      copiedText: '复制成功'
+    })
+  ]
 })
